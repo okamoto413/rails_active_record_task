6e668b18adf41f9d1095d364d7343cb4d9f43fff
kujira12 <ha9botan@gmail.com>
2024-04-03 18:21:52 +0900

    2・3・4問目　課題再提出

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -9,7 +9,7 @@ class ExercisesController < ApplicationController
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
     #   * left_outer_joinsを使うこと
-     @shops = Shop.eager_load(foods: :order_foods).where(order_foods: { food_id: nil })
+     @shops = Shop.eager_load(foods: :order_foods).where(order_foods: { food_id: nil }).distinct
   end
 
   def exercise3 
@@ -18,9 +18,8 @@ class ExercisesController < ApplicationController
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
   @address = Address.joins(:orders)
                     .group('addresses.id')
-                    .order('COUNT(orders.customer_id) DESC')
-                    .eager_load(:orders)
-                    .select('addresses.*, COUNT(orders.customer_id) AS orders_count')
+                    .order('COUNT(orders.id) DESC')
+                    .select('addresses.*, COUNT(orders.id) AS orders_count')
                     .first
   end
 
@@ -28,7 +27,7 @@ class ExercisesController < ApplicationController
     # 【要件】一番お金を使っている顧客を返すこと
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
-    @customer = Customer.eager_load(orders: { order_foods: :food })
+    @customer = Customer.joins(orders: { order_foods: :food })
                         .group('customers.id')
                         .order('SUM(foods.price) DESC')
                         .select('customers.*, SUM(foods.price) AS foods_price_sum')
