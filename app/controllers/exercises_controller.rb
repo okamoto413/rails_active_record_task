9bd44f98bd3dbf254d8bd61b2a22238fc58f2ac3
kujira12 <ha9botan@gmail.com>
2024-04-03 18:29:48 +0900

    2問目　課題再提出

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -9,7 +9,7 @@ class ExercisesController < ApplicationController
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
     #   * left_outer_joinsを使うこと
-     @shops = Shop.eager_load(foods: :order_foods).where(order_foods: { food_id: nil }).distinct
+     @shops = Shop.left_outer_joins(foods: :order_foods).where(order_foods: { food_id: nil }).distinct
   end
 
   def exercise3 
